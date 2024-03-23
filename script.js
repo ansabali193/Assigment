91728731dc250a5001a54e807ae8c73ba58a9a51
Ansab Ali <157951177+ansabali193@users.noreply.github.com>
2024-03-23 09:09:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+//Chapter 2
+
+//Question # 1:
+let username;
+//
+//Question # 2:
+let myName = "Ansab Ali";
+//
+//Question # 3:
+let message;
+message = "Hello world";
+alert(message);
+//
+//Question # 4:
+let myname = "Ansab Ali";
+let Age = 22;
+let Qualification = "DAE Engineering";
+let Hobbies = "Football and Coding";
+
+//display in the alert box
+let Mybiodata = "(My Bio Data)";
+Mybiodata += "Name: " + myname + "\n";
+Mybiodata += "Age: " + Age + "\n";
+Mybiodata += "Qualification: " + Qualification + "\n";
+Mybiodata += "Hobbies: " + Hobbies + "\n";
+alert(Mybiodata);
+//
+// Question # 5:
+alert("pizza\npizz\npiz\npi\np");
+
+//Question # 6:
+let email = "ansabali193@gmail.com";
+let emailaddress = "My email address is: '" + email + "'";
+alert(emailaddress);
+//
+
+//Question # 7:
+let book = "A smarter way to learn JavaScript";
+
+//display in the alert box
+alert("I am currently reading the book: '" + book + "'");
+//
+
+//Question # 8:
+document.write("Yah! I can write HTML content through JavaScript");
+//
+
+//Question # 9:
+let specialstring = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// Display the string in an alert box
+alert(specialstring);
+
+// Display the string in the browser
+document.write(specialstring);
+//
+
+// Chapter # 4:
+// Question 1;
+let variable1, variable2, variable3;
+
+//Question # 2
+//legal variable
+/*let Ansab Ali;
+let Ansab Ali;
+let $Ansab Ali;
+let _Ansab Ali;
+let An99sab Ali;
+//
+
+//illegal variable
+let ansab ali
+let 99ansab ali
+let "ansab ali"
+let @ansab ali
+let (ansab ali)*/
+//
+
+//Question # 3
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write(
+  "<p></p>Variable names can only contain ,numbers,$ and _ . For example : $my_1stVariable</p>"
+);
+document.write(
+  "<p>Variables must begin with a letter,$ or_ . For example : $name, _name or name</p>"
+);
+document.write("<p></p>Variable names are case-sensitive</p>");
+document.write("<p></p>Variable names should not be JavaScript keywords</p>");
+//
